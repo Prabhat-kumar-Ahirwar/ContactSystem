22996d61e8dcb863e428f3270a1c2bfc130f83b0
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-05 19:05:59 +0530

    Initial Commit

---
+++ src/main/java/com/example/ContactSystem/ContactSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContactSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactSystemApplication.class, args);
+	}
+
+}
