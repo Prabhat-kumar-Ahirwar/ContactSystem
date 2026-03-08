da08a90db7dea44d8c9a570f33ab3ec6446615f3
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-08 21:48:30 +0530

    Adding proper validation in project

--- src/main/java/com/example/ContactSystem/Entity/Contacts.java
+++ src/main/java/com/example/ContactSystem/Entity/Contacts.java
@@ -3,6 +3,7 @@ package com.example.ContactSystem.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.processing.Pattern;
 
 @Entity
 @Getter
@@ -14,7 +15,7 @@ public class Contacts {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     private long number;
 
     @Column(nullable = false, length = 50)
